388d6faef3e7ba23d6fe1d1e40e22033f3d18efa
Armand Minassian <am110953@ssh.sandbox.csun.edu>
2021-02-14 19:13:28 -0800

    update 3 working on bugfixes

--- checksum.c
+++ checksum.c
@@ -19,27 +19,35 @@ int main (int argc, char * argv[], char ** envp) {
 
   int count = 10;
   int sum = 0;   
-  char checksum;
-  char complement;
+  unsigned char checksum;
+  unsigned char complement;
   char header[10];
+  int quotient = 0;
+  int remainder = 0;
 
   read(0, header, 10);
 
   for (int i = 0; i < count; i++) {
 
-    header[i] = (int) getchar();
+    	unsigned char temp = (unsigned char) header[i];
 
         if (i == 5) {
-            checksum = header[i];
+            checksum = temp;
         }
         else {
-            sum = sum + header[i];
+            sum = sum + temp;
         }
+        fprintf(stdout, "DEBUG OF TEMP: %d, DEBUG OF SUM %d\n", temp, sum);
   }
 
+  quotient = sum / (max_int + 1);
+  remainder = sum % (max_int + 1);
+  sum = quotient + remainder;
   complement = max_int - sum;
 
-  fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
+fprintf(stdout, "%d, %d\n", max_int, sum);
+
+ fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
 
   if (checksum != complement ) {
     fprintf(stderr, "Error Detected!\n"); 
